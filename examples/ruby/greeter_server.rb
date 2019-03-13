56d0345b0c14323cc562582189cda1daea61a44a
Andrey Khataev <andrey.hataev@balance-pl.ru>
2019-03-13 07:27:07 +0300

    impr

--- examples/ruby/greeter_server.rb
+++ examples/ruby/greeter_server.rb
@@ -29,8 +29,8 @@ require 'helloworld_services_pb'
 class GreeterServer < Helloworld::Greeter::Service
   # say_hello implements the SayHello rpc method.
   def say_hello(hello_req, _unused_call)
-    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
     p "called say_hello"
+    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
   end
   def say_hello_again(hello_req, _unused_call)
     Helloworld::HelloReply.new(message: "Hello Again #{hello_req.name}")
